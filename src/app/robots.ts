d8a281d516cba6d4b2bf4315ad34a5de973c9bdb
Manuel <yesas12345678@gmail.com>
2026-06-25 09:19:49 +0200

    fix: update sitemap domain in robots.ts to .xyz

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://noticiasmundial.com';
+  const baseUrl = 'https://noticiasmundial.xyz';
   
   return {
     rules: {
